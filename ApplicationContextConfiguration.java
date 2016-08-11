19e31a25ec14a4f044d708587fe675ee4879bdd2
eswarkakinada <eswar.kakinada@gmail.com>
2016-08-11 18:20:59 +0530

    Add files via upload

--- ApplicationContextConfiguration.java
+++ ApplicationContextConfiguration.java
@@ -2,71 +2,69 @@ package com.niit.shoppinngcart.configuration;
 import java.util.Properties;
 
 import javax.sql.DataSource;
- 
+
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
- 
+
+import com.niit.shoppingcart.model.Category;
+import com.niit.shoppingcart.model.Supplier;
 
 @Configuration
-
+@ComponentScan("com.niit.shoppingcartback")
 @EnableTransactionManagement
-@ComponentScan({ "com.niit.shoppinngcart" })
-@PropertySource(value = { "classpath:application.properties" })
+
 
 
 public class ApplicationContextConfiguration {
+
+	@Bean(name = "dataSource")
+	public DataSource getDataSource() {
+	    BasicDataSource dataSource = new BasicDataSource();
+	    dataSource.setDriverClassName("org.h2.driver");
+	    dataSource.setUrl("jdbc:h2:tcp://localhost/~/Eswar"); 
+	    dataSource.setUsername("sa");
+	    dataSource.setPassword("");
 	 
-	    @Autowired
-	    private Environment environment;
+	    return dataSource;
+	}
+	private Properties getHibernateProperties() {
+	    Properties properties = new Properties();
+	    properties.put("hibernate.show_sql", "true");
+	    properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+	    properties.put("hibernate.hbm2dll.auto", "update");
+	    return properties;
+	}
+	@Autowired
+	@Bean(name="sessionfactory")
+	public SessionFactory getSessionFactory(DataSource dataSource) {
+		 
+	    LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+	    sessionBuilder.addProperties(getHibernateProperties());
 	 
-	    @Bean
-	    public LocalSessionFactoryBean sessionFactory() {
-	        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-	        sessionFactory.setDataSource(dataSource());
-	        sessionFactory.setPackagesToScan(new String[] { "com.niit.shoppingcart" });
-	        sessionFactory.setHibernateProperties(hibernateProperties());
-	        return sessionFactory;
-	     }
-	     
-	    @Bean
-	    public DataSource dataSource() {
-	        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	        dataSource.setDriverClassName(environment.getRequiredProperty("org.h2.driver"));
-	        dataSource.setUrl(environment.getRequiredProperty("jdbc:h2:tcp://localhost/~/Eswar"));
-	        dataSource.setUsername(environment.getRequiredProperty("sa"));
-	        dataSource.setPassword(environment.getRequiredProperty(""));
-	        return dataSource;
-	    }
-	     
-	    private Properties hibernateProperties() {
-	        Properties properties = new Properties();
-	        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-	        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-	        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
-	        return properties;        
-	    }
-	     
-	    @Bean
-	    @Autowired
-	    public HibernateTransactionManager transactionManager(SessionFactory s) {
-	       HibernateTransactionManager txManager = new HibernateTransactionManager();
-	       txManager.setSessionFactory(s);
-	       return txManager;
-	    }
+	    sessionBuilder.addAnnotatedClasses(Category.class);
+	    sessionBuilder.addAnnotatedClass(Supplier.class);
+	 
+	    return sessionBuilder.buildSessionFactory();
 	}
 	
+	@Autowired
+	@Bean(name="transactionmanager")
+	public HibernateTransactionManager getTransactionManager(
+	        SessionFactory sessionFactory) {
+	    HibernateTransactionManager transactionManager = new HibernateTransactionManager(
+	            sessionFactory);
+	 
+	    return transactionManager;
+	}
 	
-	
-	
+}
 	
 	
 	
---
+++ UserDetails.java
@@ -0,0 +1,57 @@
+package com.niit.shoppingcart.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table
+@Component
+public class UserDetails {
+
+	private String id;
+	private String name;
+	private String emailid;
+	
+	private String password;
+	private String contact;
+	private String address;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmailid() {
+		return emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
