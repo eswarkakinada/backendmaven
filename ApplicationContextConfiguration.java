61547a3cc78a57db990ce17f28fb8e58ac979a25
eswarkakinada <eswar.kakinada@gmail.com>
2016-08-29 19:34:17 +0530

    new version
    package com.niit.shoppinngcart.configuration;
    
    import java.util.Properties;
    
    import javax.sql.DataSource;
    
    import org.apache.commons.dbcp2.BasicDataSource;
    import org.hibernate.SessionFactory;
    import org.springframework.beans.factory.annotation.Autowired;
    import org.springframework.context.annotation.Bean;
    import org.springframework.context.annotation.ComponentScan;
    import org.springframework.context.annotation.Configuration;
    import org.springframework.orm.hibernate4.HibernateTransactionManager;
    import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
    import org.springframework.transaction.annotation.EnableTransactionManagement;
    
    import com.niit.shoppingcart.dao.CategoryDAO;
    import com.niit.shoppingcart.dao.CategoryDAOImpl;
    import com.niit.shoppingcart.dao.ProductDAO;
    import com.niit.shoppingcart.dao.ProductDAOImpl;
    import com.niit.shoppingcart.dao.SupplierDAO;
    import com.niit.shoppingcart.dao.SupplierDAOImpl;
    import com.niit.shoppingcart.dao.UserDetailsDAO;
    import com.niit.shoppingcart.dao.UserDetailsDAOImpl;
    import com.niit.shoppingcart.model.Category;
    import com.niit.shoppingcart.model.Product;
    import com.niit.shoppingcart.model.Supplier;
    import com.niit.shoppingcart.model.UserDetails;
    
    
    
    @Configuration
    @ComponentScan
    @EnableTransactionManagement
    public class ApplicationContextConfig {
    
    	@Bean(name = "dataSource")
    	public DataSource getDataSource() {
    
    		BasicDataSource dataSource = new BasicDataSource();
    		dataSource.setDriverClassName("org.h2.Driver");
    		dataSource.setUrl("jdbc:h2:tcp://localhost/~/Eswar");
    		dataSource.setUsername("sa");
    		dataSource.setPassword("");
    
    		return dataSource;
    	}
    
    	private Properties getHibernateProperties() {
    		Properties properties = new Properties();
    		properties.put("hibernate.show_sql", "true");
    		properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
    		properties.put("hibernate.hbm2dll.auto","Update");
    	properties.put("hibernate.current_session_context_class","thread");
    		return properties;
    	}
    
    	@Autowired
    	@Bean(name = "sessionFactory")
    	public SessionFactory getSessionFactory(DataSource dataSource) {
    
    		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
    		sessionBuilder.addProperties(getHibernateProperties());
    
    		sessionBuilder.addAnnotatedClass(Category.class);
    		sessionBuilder.addAnnotatedClass(Supplier.class);
    		sessionBuilder.addAnnotatedClass(Product.class);
    		sessionBuilder.addAnnotatedClass(UserDetails.class);
    		return sessionBuilder.buildSessionFactory();
    	}
    
    	@Autowired
    	@Bean(name = "transactionmanager")
    	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
    		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
    
    		return transactionManager;
    	}
    
    	 @Autowired
    	 
    	 @Bean(name = "categoryDAO")
    	 public CategoryDAO getCategoryDAO(SessionFactory sessionFactory) {
    		 return new CategoryDAOImpl(sessionFactory);
    		 }
    	
    
    	@Autowired
    	@Bean(name = "ProductDAO")
    	public ProductDAO getProductDAO(SessionFactory sessionFactory) {
    		return new ProductDAOImpl(sessionFactory);
    	}
    
    	
    	@Autowired
    	@Bean(name = "product")
    	public Product getProduct (){
    	return new Product();
    	}
    	@Autowired
    	@Bean(name = "SupplierDAO")
    	public SupplierDAO getSupplierDAO(SessionFactory sessionFactory) {
    		return new SupplierDAOImpl(sessionFactory);
    	}
    	
    	@Autowired
    	@Bean(name = "supplier")
    	public Supplier getSupplier (){
    	return new Supplier();
    	}
    
    	@Autowired
    	@Bean(name = "UserDetailsDAO")
    	public UserDetailsDAO getUserDetailsDAO(SessionFactory sessionFactory) {
    
    	return new UserDetailsDAOImpl(sessionFactory);
    	}
    	@Autowired
    	@Bean(name = "userDetails")
    	public UserDetails getUserDetails (){
    	return new UserDetails();
    	}
    	
    
    }

--- ApplicationContextConfiguration.java
+++ ApplicationContextConfiguration.java
@@ -1,4 +1,5 @@
 package com.niit.shoppinngcart.configuration;
+
 import java.util.Properties;
 
 import javax.sql.DataSource;
@@ -13,59 +14,112 @@ import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.niit.shoppingcart.dao.CategoryDAO;
+import com.niit.shoppingcart.dao.CategoryDAOImpl;
+import com.niit.shoppingcart.dao.ProductDAO;
+import com.niit.shoppingcart.dao.ProductDAOImpl;
+import com.niit.shoppingcart.dao.SupplierDAO;
+import com.niit.shoppingcart.dao.SupplierDAOImpl;
+import com.niit.shoppingcart.dao.UserDetailsDAO;
+import com.niit.shoppingcart.dao.UserDetailsDAOImpl;
 import com.niit.shoppingcart.model.Category;
+import com.niit.shoppingcart.model.Product;
 import com.niit.shoppingcart.model.Supplier;
+import com.niit.shoppingcart.model.UserDetails;
+
+
 
 @Configuration
-@ComponentScan("com.niit.shoppingcartback")
+@ComponentScan
 @EnableTransactionManagement
-
-
-
-public class ApplicationContextConfiguration {
+public class ApplicationContextConfig {
 
 	@Bean(name = "dataSource")
 	public DataSource getDataSource() {
-	    BasicDataSource dataSource = new BasicDataSource();
-	    dataSource.setDriverClassName("org.h2.driver");
-	    dataSource.setUrl("jdbc:h2:tcp://localhost/~/Eswar"); 
-	    dataSource.setUsername("sa");
-	    dataSource.setPassword("");
-	 
-	    return dataSource;
+
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:tcp://localhost/~/Eswar");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+
+		return dataSource;
 	}
+
 	private Properties getHibernateProperties() {
-	    Properties properties = new Properties();
-	    properties.put("hibernate.show_sql", "true");
-	    properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-	    properties.put("hibernate.hbm2dll.auto", "update");
-	    return properties;
+		Properties properties = new Properties();
+		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		properties.put("hibernate.hbm2dll.auto","Update");
+	properties.put("hibernate.current_session_context_class","thread");
+		return properties;
 	}
+
 	@Autowired
-	@Bean(name="sessionfactory")
+	@Bean(name = "sessionFactory")
 	public SessionFactory getSessionFactory(DataSource dataSource) {
-		 
-	    LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
-	    sessionBuilder.addProperties(getHibernateProperties());
+
+		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+		sessionBuilder.addProperties(getHibernateProperties());
+
+		sessionBuilder.addAnnotatedClass(Category.class);
+		sessionBuilder.addAnnotatedClass(Supplier.class);
+		sessionBuilder.addAnnotatedClass(Product.class);
+		sessionBuilder.addAnnotatedClass(UserDetails.class);
+		return sessionBuilder.buildSessionFactory();
+	}
+
+	@Autowired
+	@Bean(name = "transactionmanager")
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+
+		return transactionManager;
+	}
+
+	 @Autowired
 	 
-	    sessionBuilder.addAnnotatedClasses(Category.class);
-	    sessionBuilder.addAnnotatedClass(Supplier.class);
-	 
-	    return sessionBuilder.buildSessionFactory();
+	 @Bean(name = "categoryDAO")
+	 public CategoryDAO getCategoryDAO(SessionFactory sessionFactory) {
+		 return new CategoryDAOImpl(sessionFactory);
+		 }
+	
+
+	@Autowired
+	@Bean(name = "ProductDAO")
+	public ProductDAO getProductDAO(SessionFactory sessionFactory) {
+		return new ProductDAOImpl(sessionFactory);
+	}
+
+	
+	@Autowired
+	@Bean(name = "product")
+	public Product getProduct (){
+	return new Product();
+	}
+	@Autowired
+	@Bean(name = "SupplierDAO")
+	public SupplierDAO getSupplierDAO(SessionFactory sessionFactory) {
+		return new SupplierDAOImpl(sessionFactory);
 	}
 	
 	@Autowired
-	@Bean(name="transactionmanager")
-	public HibernateTransactionManager getTransactionManager(
-	        SessionFactory sessionFactory) {
-	    HibernateTransactionManager transactionManager = new HibernateTransactionManager(
-	            sessionFactory);
-	 
-	    return transactionManager;
+	@Bean(name = "supplier")
+	public Supplier getSupplier (){
+	return new Supplier();
+	}
+
+	@Autowired
+	@Bean(name = "UserDetailsDAO")
+	public UserDetailsDAO getUserDetailsDAO(SessionFactory sessionFactory) {
+
+	return new UserDetailsDAOImpl(sessionFactory);
+	}
+	@Autowired
+	@Bean(name = "userDetails")
+	public UserDetails getUserDetails (){
+	return new UserDetails();
 	}
 	
+
 }
-	
-	
-	
-	
